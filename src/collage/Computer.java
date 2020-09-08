50d3e80488f31f9afb7a297b9a444a12ece350ec
arham2008 <akbarmunner@gmail.com>
2020-09-08 14:19:17 -0400

    i add project

---
+++ src/collage/Computer.java
@@ -0,0 +1,18 @@
+package collage;
+
+public class Computer {
+
+	public static void main(String[] args) {
+		
+		System.out.println("govt millat high school");
+		System.out.println(" i love to read and write");
+		System.out.println("do not lie to each other");
+		System.out.println("java is very hard ");
+		System.out.println(" dont trust any one");
+		
+		
+		
+		
+	}
+
+}
